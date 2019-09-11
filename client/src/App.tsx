a28acbbe108afb545883f16a0a4835d47759f452
G <g@Gs-MacBook-Pro.fios-router.home>
2019-09-10 22:16:26 -0400

    Initial setup.

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './css/main.css';
+import './App.css';
+
+export default function App() {
+	return (
+		<div id="app">
+			<p>React App Template</p>
+		</div>
+	);
+}
